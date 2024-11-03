b4ae86aee1466a413630a5eef783f209bd0633d3
NarciSource <jwch11@gmail.com>
2024-11-03 14:38:14 +0900

    refactor: pages.auth로 폴더 분리

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import AuthPage from "../pages/AuthPage";
+import AuthPage from "../pages/auth/AuthPage";
 
 const router = createBrowserRouter([
     {
