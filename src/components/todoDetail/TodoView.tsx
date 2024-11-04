dd0825a03d4e0a7749b8925f31450bc22e34881c
NarciSource <jwch11@gmail.com>
2024-11-04 21:34:36 +0900

    style: TodoView 수정 모드

--- src/components/todoDetail/TodoView.tsx
+++ src/components/todoDetail/TodoView.tsx
@@ -1,25 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
-import {
-    Box,
-    Flex,
-    Heading,
-    IconButton,
-    PopoverArrow,
-    PopoverBody,
-    PopoverContent,
-    PopoverRoot,
-    PopoverTitle,
-    PopoverTrigger,
-    Stack,
-    Text,
-} from "@chakra-ui/react";
+import { Box, Flex, Heading, IconButton, Stack, Text } from "@chakra-ui/react";
 import { useQuery } from "@tanstack/react-query";
 import { FaPencil } from "react-icons/fa6";
 
 import { TodoResponse } from "@/api/todoApi";
 import readTodo from "@/api/services/todo/readTodo";
 import TodoFormLayout from "@/components/todoForm/TodoFormLayout";
+import { FaSearch } from "react-icons/fa";
 
 export default function TodoView({ todoId }: { todoId: string }): React.ReactElement {
     const { data } = useQuery<TodoResponse>({
@@ -31,50 +19,60 @@ export default function TodoView({ todoId }: { todoId: string }): React.ReactEle
         staleTime: 1000 * 60 * 5,
     });
 
+    const [isEditable, setIsEditable] = useState(false);
+
+    const toggleEditable = () => {
+        setIsEditable((prev) => !prev);
+    };
+
     return (
         <>
             {data && (
                 <Box>
-                    <PopoverRoot>
-                        <PopoverTrigger asChild>
-                            <Flex justify="flex-end">
-                                <IconButton
-                                    type="submit"
-                                    position="absolute"
-                                    mt={5}
-                                    colorPalette="blue"
-                                    size="sm"
-                                    bg={{ base: "colorPalette.600", _dark: "colorPalette.400" }}
-                                >
-                                    <FaPencil />
-                                </IconButton>
+                    <IconButton
+                        onClick={toggleEditable}
+                        mt={5}
+                        colorPalette="blue"
+                        size="sm"
+                        bg={{ base: "colorPalette.600", _dark: "colorPalette.400" }}
+                    >
+                        {isEditable ? <FaSearch /> : <FaPencil />}
+                    </IconButton>
+
+                    {isEditable ? (
+                        <TodoFormLayout id={data.id} title={data.title} content={data.content} />
+                    ) : (
+                        <Box
+                            overflow="hidden"
+                            textOverflow="ellipsis"
+                            borderWidth={1}
+                            borderColor="gray.300"
+                            borderRadius="md"
+                            boxShadow="md"
+                            mt={3}
+                            p={5}
+                        >
+                            <Heading paddingTop={5}>{data.title}</Heading>
+
+                            <Text p={10}>{data.content}</Text>
+
+                            <Flex
+                                justify="flex-end"
+                                borderTopWidth={1}
+                                borderColor="gray.300"
+                                pt={2}
+                            >
+                                <Stack textStyle="xs">
+                                    <Text>
+                                        생성시간: {new Date(data.createdAt).toLocaleString()}
+                                    </Text>
+                                    <Text>
+                                        수정시간: {new Date(data.updatedAt).toLocaleString()}
+                                    </Text>
+                                </Stack>
                             </Flex>
-                        </PopoverTrigger>
-
-                        <PopoverContent>
-                            <PopoverArrow />
-                            <PopoverBody>
-                                <PopoverTitle fontWeight="medium">ToDo 추가</PopoverTitle>
-                                <TodoFormLayout
-                                    id={data.id}
-                                    title={data.title}
-                                    content={data.content}
-                                />
-                            </PopoverBody>
-                        </PopoverContent>
-                    </PopoverRoot>
-                    <Stack overflow="hidden" textOverflow="ellipsis">
-                        <Heading paddingTop={5}>{data.title}</Heading>
-
-                        <Text p={10}>{data.content}</Text>
-
-                        <Flex justify="flex-end">
-                            <Stack textStyle="xs">
-                                <Text>생성시간: {new Date(data.createdAt).toLocaleString()}</Text>
-                                <Text>수정시간: {new Date(data.updatedAt).toLocaleString()}</Text>
-                            </Stack>
-                        </Flex>
-                    </Stack>
+                        </Box>
+                    )}
                 </Box>
             )}
         </>
