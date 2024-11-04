a58109f9e3ca6429264b9c753ff4e457c25e5ed0
NarciSource <jwch11@gmail.com>
2024-11-05 00:13:37 +0900

    style: 프론트페이지

--- src/components/layouts/CommonLayout.tsx
+++ src/components/layouts/CommonLayout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, Image } from "@chakra-ui/react";
 import { Toaster } from "@/components/ui/toaster";
 
 import Navbar from "./Navbar";
@@ -18,10 +18,14 @@ export default function CommonLayout({ children }: CommonLayoutProps): React.Rea
             <Flex justify="center">
                 <Navbar />
             </Flex>
-            {isLoggedIn && (
+            {isLoggedIn ? (
                 <Box px={{ base: 4 }} pt="24" paddingInline="15%" minH="100vh">
                     {children}
                 </Box>
+            ) : (
+                <Flex justify="center" align="center">
+                    <Image src={`/images/logo.png`} alt="logo" align="center" w={700} />
+                </Flex>
             )}
             <Toaster />
         </div>
