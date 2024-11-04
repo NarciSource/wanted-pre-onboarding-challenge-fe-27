f261e9d60c3ba070d9d11782f4f0431ca95a3611
NarciSource <jwch11@gmail.com>
2024-11-04 22:33:30 +0900

    style: Navbar 백그라운드

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -1,14 +1,26 @@
 import React from "react";
 
 import { Box, Flex, Link, Stack } from "@chakra-ui/react";
-import { useColorModeValue } from "../ui/color-mode";
+import { useColorModeValue } from "@/components/ui/color-mode";
+
 import Popover from "./Popover";
-import LoginFormLayout from "../auth/LoginFormLayout";
-import SignUpFormLayout from "../auth/SignUpFormLayout";
+import LoginFormLayout from "@/components/auth/LoginFormLayout";
+import SignUpFormLayout from "@/components/auth/SignUpFormLayout";
 
 export default function Navbar(): React.ReactElement {
     return (
-        <Box zIndex={10} position="fixed" left={0} width="100%" py={{ base: 2 }} px={{ base: 4 }}>
+        <Box
+            zIndex={10}
+            position="fixed"
+            left={0}
+            width="100%"
+            py={{ base: 2 }}
+            px={{ base: 4 }}
+            bg={useColorModeValue("white", "white")}
+            borderBottomWidth={1}
+            borderColor="gray.300"
+            boxShadow="sd"
+        >
             <Flex maxW="960px" minH="60px" align="center" m="auto">
                 <Flex justify="center" flex={{ base: 1, md: "auto" }}>
                     <Link
