167f0575376008e2e936ddf0870fa13ecb03fa5c
NarciSource <jwch11@gmail.com>
2024-11-04 19:00:00 +0900

    style: TodoCardList 형태 변환
    - 행우선 행렬로 쌓이게
    - 첫번째는 추가 카드

--- src/components/todoCard/TodoCardList.tsx
+++ src/components/todoCard/TodoCardList.tsx
@@ -1,24 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import {
-    Box,
-    For,
-    PopoverArrow,
-    PopoverBody,
-    PopoverContent,
-    PopoverRoot,
-    PopoverTitle,
-    PopoverTrigger,
-    Stack,
-} from "@chakra-ui/react";
-import { Button } from "@/components/ui/button";
+import { Box, Card, Flex, For } from "@chakra-ui/react";
 import { useQuery } from "@tanstack/react-query";
 
 import { TodoError, TodoListResponse } from "@/api/todoApi";
 import readAllTodo from "@/api/services/todo/readAllTodo";
 import TodoFormLayout from "@/components/todoForm/TodoFormLayout";
-import TodoItemCard from "./TodoCardItem";
+import TodoCardItem from "./TodoCardItem";
 
 export default function TodoCardList(): React.ReactElement {
     const { data } = useQuery<undefined, TodoError, TodoListResponse>({
@@ -28,30 +17,23 @@ export default function TodoCardList(): React.ReactElement {
 
     return (
         <Box>
-            <PopoverRoot>
-                <PopoverTrigger asChild>
-                    <Button size="sm" variant="outline">
-                        ToDo 추가
-                    </Button>
-                </PopoverTrigger>
-                <PopoverContent>
-                    <PopoverArrow />
-                    <PopoverBody>
-                        <PopoverTitle fontWeight="medium">ToDo 추가</PopoverTitle>
+            <Flex direction="row" wrap="wrap" justify="flex-start">
+                <Card.Root variant="elevated">
+                    <Card.Header>
+                        <Card.Title>추가</Card.Title>
+                    </Card.Header>
+                    <Card.Body>
                         <TodoFormLayout />
-                    </PopoverBody>
-                </PopoverContent>
-            </PopoverRoot>
-
-            <Stack>
-                <For each={data}>
+                    </Card.Body>
+                </Card.Root>
+                <For each={data?.slice().reverse()}>
                     {(each) => (
                         <Link to={`/detail/${each.id}`}>
-                            <TodoItemCard {...each} key={each.id} />
+                            <TodoCardItem {...each} key={each.id} />
                         </Link>
                     )}
                 </For>
-            </Stack>
+            </Flex>
         </Box>
     );
 }
