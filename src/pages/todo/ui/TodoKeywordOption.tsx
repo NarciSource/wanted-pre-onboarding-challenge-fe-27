2872808579ed03223856dacda558305cbc2e24ee
NarciSource <jwch11@gmail.com>
2024-11-10 15:41:21 +0900

    feat: 키워드 옵션 연동

---
+++ src/pages/todo/ui/TodoKeywordOption.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { LuSearch } from "react-icons/lu";
+
+import { InputGroup } from "@/widgets/chakra-ui/input-group";
+import { Input } from "@chakra-ui/react";
+
+interface Parameters {
+    handleOptionChange: (name: string, value: string | null) => void;
+}
+
+export default function TodoKeywordOption({ handleOptionChange }: Parameters): React.ReactElement {
+    return (
+        <InputGroup startElement={<LuSearch />} w="150px">
+            <Input
+                placeholder="키워드 검색"
+                onChange={(e) => {
+                    handleOptionChange("keyword", e.target.value);
+                }}
+            />
+        </InputGroup>
+    );
+}
--- src/pages/todo/ui/TodoPageOptions.tsx
+++ src/pages/todo/ui/TodoPageOptions.tsx
@@ -6,11 +6,13 @@ import { HStack, Icon } from "@chakra-ui/react";
 import TodoFilterOption from "./TodoFilterOption";
 import TodoOrderOption from "./TodoOrderOption";
 import TodoSortOption from "./TodoSortOption";
+import TodoKeywordOption from "./TodoKeywordOption";
 
 type Options = {
     sort: "createdAt" | "updatedAt" | "priority";
     order: "asc" | "desc";
     priorityFilter?: "urgent" | "normal" | "low";
+    keyword?: string;
 };
 
 export default function TodoPageOptions({
@@ -28,7 +30,7 @@ export default function TodoPageOptions({
     };
 
     return (
-        <HStack justify="center">
+        <HStack justify="center" mb={5}>
             <TodoSortOption sort={options.sort} handleOptionChange={handleOptionChange} />
             <Icon color="grey" mx={7}>
                 <RxDividerVertical />
@@ -43,6 +45,11 @@ export default function TodoPageOptions({
                 priorityFilter={options.priorityFilter}
                 handleOptionChange={handleOptionChange}
             />
+            <Icon color="grey" mx={7}>
+                <RxDividerVertical />
+            </Icon>
+
+            <TodoKeywordOption handleOptionChange={handleOptionChange} />
         </HStack>
     );
 }
